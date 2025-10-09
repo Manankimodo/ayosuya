d51f5d553cdbdc31d71bec1ed67253408e8a0878
231057 <ikuyoshi7300@gmail.com>
2025-10-09 15:19:09 +0900

    ログイン後の管理者の確認ができて遷移することも可能になりました

--- sotuken/app.py
+++ sotuken/app.py
@@ -8,6 +8,10 @@ import chromadb
 import ollama
 import warnings
 
+
+
+#--------------------------------------------------------------------------------
+
 # 警告を非表示
 warnings.filterwarnings("ignore")
 app = Flask(__name__)
@@ -24,6 +28,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # mysql = MySQL(app)
+
+
+# 仮のユーザーデータ
+users = {
+    "user1": {"password": "1234"},
+    "admin": {"password": "adminpass"}
+}
+
+@app.route("/check")
+def check():
+    # ログイン済み確認
+    if "user_id" not in session:
+        return redirect(url_for("login"))
+    return render_template("check.html")
+
+@app.route("/admin")
+def admin():
+    return render_template ("calendar.html")
+
+@app.route("/shift")
+def shift():
+    return render_template ("login.html")
 
 
 # カレンダー表示
@@ -77,8 +103,8 @@ def login():
         result = db.session.execute(sql, {"user_id": user_id, "password": password}).fetchone()
 
         if result:
-            session["user"] = user_id
-            return redirect(url_for("calendar"))
+            session["user_id"] = user_id
+            return redirect(url_for("check"))
         else:
             flash("IDまたはパスワードが間違っています。", "danger")
     
